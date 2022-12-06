00f61a32afceaadf245ef28cda64644a2e2d71dc
SwiftyChicken <erwema25@gmail.com>
2022-12-06 15:31:15 +0100

    ADD: png to binary converter

---
+++ converter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys, getopt
+from PIL import Image
+
+palette = {(13, 13, 13): 1,
+           (40, 43, 36): 0}
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print('USAGE:\n\tconverter.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('converter.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+
+   img_png = Image.open(inputfile) 
+   pixels = img_png.load()
+   width, height = img_png.size
+   with open(outputfile, 'wb') as output:
+        for h in range(height):
+            for w in range(width):
+                r, g, b = pixels[w, h]
+                r >>= 2
+                g >>= 2
+                b >>= 2
+
+                byte = (palette[(r, g, b)]).to_bytes(2, byteorder='big')
+                output.write(byte)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
